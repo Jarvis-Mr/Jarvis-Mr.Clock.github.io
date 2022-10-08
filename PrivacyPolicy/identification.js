fe7176e78daf6bab7d94ba1e250038d8fcdfedac
Jarvis <jarvis.lzw@gmail.com>
2022-10-08 14:19:07 +0800

    增加用户协议和隐私政策

---
+++ PrivacyPolicy/identification.js
@@ -0,0 +1,53 @@
+﻿window.onload = function () {
+  function getQueryString(name) {
+    let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    let r = window.location.search.substr(1).match(reg);
+    if (r != null) {
+      return unescape(r[2]);
+    }
+    return false;
+  }
+
+  let companyList = {
+    HomeOSVensi: "威士丹利智能科技有限公司",
+  };
+
+  let company = document.getElementsByClassName("company");
+  let companyName = getQueryString("identification");
+
+  for (var i = 0; i < company.length; i++) {
+    if (!companyList[companyName]) {
+      company[i].innerHTML = companyList.HomeOSCommon;
+    } else {
+      company[i].innerHTML = companyList[companyName];
+    }
+  }
+
+  let phoneObj = {
+    HomeOSVensi: "0752-2166072",
+  };
+
+  let phones = document.getElementsByClassName("phone");
+
+  for (var i = 0; i < phones.length; i++) {
+    if (!phoneObj[companyName]) {
+      phones[i].innerHTML = phoneObj.HomeOSCommon;
+    } else {
+      phones[i].innerHTML = phoneObj[companyName];
+    }
+  }
+
+  let appNameObj = {
+    HomeOSVensi: "威士丹利智能",
+  };
+
+  let appNames = document.getElementsByClassName("appName");
+
+  for (var i = 0; i < appNames.length; i++) {
+    if (!appNameObj[companyName]) {
+      appNames[i].innerHTML = appNameObj.HomeOSCommon;
+    } else {
+      appNames[i].innerHTML = appNameObj[companyName];
+    }
+  }
+};
